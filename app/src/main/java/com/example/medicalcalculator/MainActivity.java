aa5354b15b89fb690c7dddbfcb6a1652e665731e
Mark Blue <mblue@students.mchenry.edu>
2019-02-12 12:49:16 -0600

    finished main layout coding MainActivity

--- app/src/main/java/com/example/medicalcalculator/MainActivity.java
+++ app/src/main/java/com/example/medicalcalculator/MainActivity.java
@@ -2,8 +2,19 @@ package com.example.medicalcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    double conversionRate = 2.2;
+    double weightEntered;
+    double convertedWeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +24,20 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.mipmap.ic_launcher);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        final EditText weight = (EditText)findViewById(R.id.txtWeight);
+        final RadioButton lbToKilo = (RadioButton)findViewById(R.id.radLbToKilo);
+        final RadioButton kiloToLb = (RadioButton)findViewById(R.id.radKiloToLb);
+        final TextView result = (TextView)findViewById(R.id.txtResult);
+        Button convert = (Button)findViewById(R.id.btnConvert);
+
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                weightEntered = Double.parseDouble(weight.getText().toString());
+                DecimalFormat tenth = new DecimalFormat("#,#");
+            }
+        });
+
     }
 }
